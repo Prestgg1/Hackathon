76d735be2a518a6a1a0473b2729e48187c470753
Prestgg1 <prestgg12@gmail.com>
2025-06-30 23:03:36 +0400

    Added header

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ToastdevProvider, Toastdev } from '@azadev/react-toastdev'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ToastdevProvider toastTheme='dark-ikonik'>
-      <Toastdev />
-      <App />
-    </ToastdevProvider>
+    <BrowserRouter>
+      <ToastdevProvider toastTheme='dark-ikonik' >
+        <Toastdev />
+        <App />
+      </ToastdevProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
